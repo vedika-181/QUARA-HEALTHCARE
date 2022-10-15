ab5028dab14fc3968e3f9cfd9e7671e1144a87e5
vedika-181 <71492296+vedika-181@users.noreply.github.com>
2022-10-15 20:18:06 +0530

    Add files via upload

---
+++ Public/Login/Login-SignUp.js
@@ -0,0 +1,40 @@
+var x=document.getElementById("login")
+        var y=document.getElementById("register")
+        var z=document.getElementById("btn")
+
+        function register(){
+            x.style.left = "-400px";
+            y.style.left = "50px";
+            z.style.left = "110px";
+        } 
+        function login(){
+            x.style.left = "50px";
+            y.style.left = "450px";
+            z.style.left = "0px";
+        }
+          
+        function myFunction() {
+          var x = document.getElementById("password");
+          if (x.type === "password") {
+            x.type = "text";
+          } else {
+            x.type = "password";
+          }
+          
+        }
+
+        function myFunction1() {
+          var x = document.getElementById("password");
+          if (x.type === "password") {
+            x.type = "text";
+          } else {
+            x.type = "password";
+          }
+          
+        }
+
+          
+          
+            
+
+            
