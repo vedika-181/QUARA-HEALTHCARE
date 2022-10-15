16513956d965b4871fa3d54706a5a9d9d5665adc
vedika-181 <71492296+vedika-181@users.noreply.github.com>
2022-10-15 20:16:36 +0530

    Add files via upload

---
+++ Public/Form/form.js
@@ -0,0 +1,229 @@
+AOS.init();
+
+// Google Maps //
+
+function initMap() {
+    /*var uluru = {lat: 42.6950218, lng: 23.2929149};
+    var mapCenter = {lat: 42.6750218, lng: 23.2840000};*/
+    var uluru = {lat: 12.9537, lng: 77.5777};
+    var mapCenter = {lat: 12.9540, lng: 76.4567};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: mapCenter,
+        scrollwheel:false,
+        styles: [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#242f3e"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#746855"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#242f3e"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#d59563"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#d59563"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#263c3f"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#6b9a76"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#38414e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#212a37"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9ca5b3"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#746855"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#1f2835"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#f3d19c"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#2f3948"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#d59563"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#17263c"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#515c6d"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#17263c"
+                    }
+                ]
+            }
+        ]
+    });
+
+    var contentString = '<div class="maptooltip center" id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h6 id="firstHeading" class="firstHeading">quarahealthcare</h6>'+
+        '<h6 id="firstHeading" class="firstHeading">Bangalore</h6>'+
+        '<div id="bodyContent">'+
+        '<em>20th cross</em>'+
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        animation: google.maps.Animation.DROP,
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+    infowindow.open(map, marker);
+};
+
+initMap().set(initMap);
+
+function redirect(){
+    window.location="../Home/Home.html"
+  }
+
+  function sendData(){
+    
+
+    fetch('http://localhost:3000/SignUp', {
+      method: 'POST',
+      body: JSON.stringify({
+          name: document.getElementById('name').value,
+          email: document.getElementById('email').value,
+          msg: document.getElementById('msg').value,
+         
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).then((response) => {
+      window.location.href = '/Home/Home.html'
+    }).catch((e) => {
+      console.log(e)
+    })
+    redirect();
+  }
